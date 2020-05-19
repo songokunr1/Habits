ff28717db2ca915e09ed15b3d431306c87705d02
songokunr1 <pawelm18@gmail.com>
2020-05-19 22:57:21 +0200

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,16 @@
-from app.models import Category, Habit, Date, Database
-from flask import render_template, url_for, flash, redirect, request
-from app import app, db
-from app.forms import New_category, New_habit, Building_habit
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta, date
+from datetime import timedelta
+
+from flask import render_template, url_for, flash, redirect
 from flask_restful import Api
 
-
-
+from app import app
+from app import db
+from app.forms import New_category, New_habit, Building_habit, Delete_habit, DateHabitReport
+from app.models import Category, Habit, Date
 from app.resources import CategoryResource
+
+#todo change model to done/not done, category name in date
+
 posts = [
     {
         'author': 'Corey Schafer',
@@ -44,117 +46,116 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/vue")
-def vue():
-    return render_template('vue.html', title='About')
+@app.route("/test")
+def test():
+    return render_template('test.html')
 
-
-@app.route("/category", methods= ['POST', 'GET'])
+@app.route("/category", methods=['POST', 'GET', 'DELETE'])
 def category():
     form = New_category()
-    cat = Category.query.all()
+    form_delete = Delete_habit()
     date = Date.find_date_by_habit_id_and_date(_id=12, date='2020-05-11')
-    print(date.json(), 'nawyk one')
-    print(form)
-    print(request.method)
     for i in range(Category.query.count()):
         print(i)
-    #print(Category.statement.execute().fetchall())
-    #print(Category.query.all())
-    print(Category.find_by_id(1))
-    print(Category.find_by_id(2))
-    #print(Category.list_of_ids)
-    #print(db.session.query(Category.id).all()[1][0]) - iterowanie po row
-    print(Category.find_by_id(db.session.query(Category.id).all()[1][0]))
-    #print(Category.list_of_ids2)
-    # print(Database.list_of_ids(Category), 'Database')
-    # print(Database.find_by_id(Category, 1), 'database look by id')
-    print(db.session.query(Category.id).all()[0], 'list of ids')
-    # print(Database.list_of_column(Category, category, Database.list_of_ids(Category)))
-    #print(Category.category_column(), 'all category')
-    print(Habit.query.all(), 'lista')
-    #print(Database.column_with_id(Category, Category.category, ))
-    cate = Category.query.get_or_404(1)
-    print(form.validate_on_submit())
     form.submit()
-    print(form.validate_on_submit())
-    #print(SQLAlchemy.execute("SELECT * FROM category"))
     if form.validate_on_submit():
         print('under POST')
-        cat = Category(category=form.category.data)
-        db.session.add(cat)
-        db.session.commit()
+        new_category = Category(category=form.category.data)
+        Category.save_to_db(new_category)
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
+    if form_delete.validate_on_submit():
+        print('under DELETE')
+        print(form_delete.category_type.data, 'category type ')
+        category_to_delete = Category.find_by_id(form_delete.category_type.data)
+        print(category_to_delete, 'asdfasdf')
+        db.session.delete(category_to_delete)
+        db.session.commit()
+        flash('Your DELETE has been created!', 'success')
+        return redirect(url_for('home'))
     return render_template('category.html', title='New Category',
-                           form=form, legend='New Category')
+                           form=form, form_delete=form_delete, legend='New Category')
+
+
+@app.route("/report", methods=['POST', 'GET', 'DELETE'])
+def report():
+    form = DateHabitReport()
+    form_delete = Delete_habit()
+    dates = Date.find_habits_by_date('2020-05-11')
+    form.submit()
+    if form.validate_on_submit():
+        number_of_dates = 1
+        choosen_date = str(form.selected_date.data)
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('report_with_date', number_of_dates=number_of_dates, choosen_date=choosen_date))
+    return render_template('report.html',
+                           form=form)
+
+
+@app.route("/report/<number_of_dates>/<choosen_date>", methods=['POST', 'GET', 'DELETE'])
+def report_with_date(number_of_dates, choosen_date):
+    form = DateHabitReport()
+    all_date_objects = [Date.find_habits_by_date(single_list) for single_list in choosen_date.split(',')]
+
+    print (all_date_objects)
+    form.submit()
+    if form.validate_on_submit():
+        number_of_dates = 1 + int(number_of_dates)
+        choosen_dates = choosen_date + ',' + str(form.selected_date.data)
+        print(choosen_dates, 'ciekawe co to za string')
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('report_with_date', number_of_dates=number_of_dates, choosen_date=choosen_dates))
+    return render_template('report.html',
+                           form=form, number_of_dates=number_of_dates, choosen_date=choosen_date, all_date_objects=all_date_objects )
+
 
 @app.route("/showcat")
 def show_cat():
-    cat=Category.query.all()
+    cat = Category.query.all()
     return render_template('showcat.html', cat=cat)
 
-@app.route("/myhabit/<habits>")
-def myhabit(habits):
-    incategory = Category.query.filter_by(category=habits).first()
-    myhabits = Habit.query.filter_by(category_id=incategory.id).all()
-    #TODO lista nawyków dla danej kategori
-    return render_template('myhabit.html', myhabits=myhabits, category=habits)
+
+@app.route("/category/<name>")
+def myhabit(name):
+    allcategory = Category.find_by_category(name)
+    myhabits = Habit.find_by_category_id(allcategory.id)
+    return render_template('myhabit.html', myhabits=myhabits, category=name)
+
 
 @app.route("/index")
 def index():
-    #TODO lista nawyków dla danej kategori
+    # TODO lista nawyków dla danej kategori
     return render_template('index.html')
 
-@app.route("/new_habit", methods= ['POST', 'GET'])
+
+@app.route("/new_habit", methods=['POST', 'GET'])
 def new_habit():
     form = New_habit()
-
-    category_list = Category_resources.list_of_category()
-    choice = [(Category.query.filter_by(category=category_name).first().id, category_name) for category_name in category_list]
-    print( choice, ' Choice')
-    print(form.data, 'form full')
-    print(form.name.data, 'name data')
-    print(form.start_date.data, 'name data')
-    print(form.end_date.data, 'name data')
-    print(form.category_type.data, 'type.data')
-    print(category_list, 'category list')
-    print(form, 'form')
-    print(Category.query.first().id, 'reczna query')
-    print(Category_resources.find_by_category(Category, 'angielski').first().id, 'find by category')
-    print(request.method)
-    print(form.validate_on_submit())
-    # print(request.form.get('cat_name'), 'cat_name variable')
+    category_list = Category.list_of_category()
     form.submit()
-    print(form.category_type.type, 'category type', form.name.name, 'category name')
-    print(form.validate_on_submit())
     if form.validate_on_submit():
-        print('under POST')
-        print(form.name.data, 'name data')
-        print(form.category_type.data, 'type.data')
-        name = Habit(name=form.name.data, category_id=form.category_type.data, date_start=form.start_date.data, date_end=form.end_date.data)
-        db.session.add(name)
-        db.session.commit()
+        new_record = Habit(name=form.name.data, category_id=form.category_type.data, date_start=form.start_date.data,
+                           date_end=form.end_date.data)
+        Habit.save_to_db(new_record)
         new_habit_id = Habit.query.filter_by(name=form.name.data).first().id
         for single_date in daterange(form.start_date.data, form.end_date.data):
-            dates = Date(date=single_date, habit_id = new_habit_id)
-            db.session.add(dates)
-            db.session.commit()
-
+            dates = Date(date=single_date, habit_id=new_habit_id, category_id=form.category_type.data)
+            Date.save_to_db(dates)
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
+
     return render_template('habit.html', title='New Category',
                            form=form, legend='New Category', category_list=category_list)
+
 
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days) + 1):
         yield start_date + timedelta(n)
 
- # if form.validate_on_submit():
- #        print('under POST')
- #        cat = Category(category=form.category.data)
- #        db.session.add(cat)
- #        db.session.commit()
- #        flash('Your post has been created!', 'success')
- #        return redirect(url_for('home'))
-
+        # if form.validate_on_submit():
+        #        print('under POST')
+        #        cat = Category(category=form.category.data)
+        #        db.session.add(cat)
+        #        db.session.commit()
+        #        flash('Your post has been created!', 'success')
+        #        return redirect(url_for('home'))
